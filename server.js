355fb4e29383a5c9d60c8a2f5cbdde5cc98a2e0a
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2021-09-26 00:07:28 -0400

    Has separate show pages for each Pokemon

--- server.js
+++ server.js
@@ -12,15 +12,14 @@ app.use(express.static("public"))
 // ROUTES
 /////////////////////////
 
-
 // INDEX
 app.get('/pokemons', (req, res) => {
-res.render('index.ejs', { data: pokemon });
+    res.render('index.ejs', { data: pokemon });
 });
 
 // SHOW
-app.get('/pokemons:id', (req, res) => {
-res.render('show.ejs', { data: pokemon[req.params.id] });
+app.get('/pokemons/:id', (req, res) => {
+    res.render('show.ejs', { pokeData: pokemon[req.params.id] });
 });
 
 
