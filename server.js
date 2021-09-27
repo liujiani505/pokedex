99fae53d2dd9df7ca9c24da6e97c4085c8b82967
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2021-09-27 00:32:27 -0400

    Has the ability to edit/delete Pokemon.

--- server.js
+++ server.js
@@ -29,7 +29,26 @@ app.get('/pokemons/new', (req, res)=>{
 // DESTROY
 app.delete('/pokemons/:id', (req, res)=>{
     pokemon.splice(req.params.id, 1);
-    res.redirect('/pokemons/')
+    res.redirect('/pokemons')
+})
+
+// UPDATE
+app.put("/pokemons/:id", (req, res)=>{
+    const updatePokemon ={
+        name: req.body.name,
+        img: req.body.img,
+        type: req.body.type,
+        stats: {
+            hp: req.body.hp,
+            attack: req.body.attack,
+            defense: req.body.defense,
+            spattack: req.body.spattack,
+            spdefense: req.body.spdefense,
+            speed: req.body.speed,
+        }
+    }
+    pokemon[req.params.id] = updatePokemon;
+    res.redirect(`/pokemons/${req.params.id}`)
 })
 
 // CREATE
@@ -48,12 +67,20 @@ app.post('/pokemons', (req, res)=>{
         }
     };
     pokemon.push(newPokemon);
-    res.redirect('/pokemons/')
+    res.redirect('/pokemons')
+})
+
+// EDIT
+app.get("/pokemons/:id/edit", (req, res)=>{
+    res.render("edit.ejs", {
+        aPokemon: pokemon[req.params.id],
+        index: req.params.id,
+    })
 })
 
 // SHOW
 app.get('/pokemons/:id', (req, res) => {
-    res.render('show.ejs', { index: req.params.id, pokeData: pokemon[req.params.id] });
+    res.render('show.ejs', { index: req.params.id, aPokemon: pokemon[req.params.id] });
 });
 
 
