e19a6a033062178f1f473db1e956f9e4eac516b4
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2021-09-26 22:04:25 -0400

    Has the ability to add a new Pokemon

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const port= 3000;
 const pokemon = require('./models/pokemon.js');
+const methodOverride = require('method-override');
 
 ////////.................
 // MIDDLEWARE
 /////////////////////////
 app.use(express.static("public"))
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'));
+
 
 /////////////////////////
 // ROUTES
@@ -17,9 +21,39 @@ app.get('/pokemons', (req, res) => {
     res.render('index.ejs', { data: pokemon });
 });
 
+// NEW
+app.get('/pokemons/new', (req, res)=>{
+    res.render('new.ejs');
+})
+
+// DESTROY
+app.delete('/pokemons/:id', (req, res)=>{
+    pokemon.splice(req.params.id, 1);
+    res.redirect('/pokemons/')
+})
+
+// CREATE
+app.post('/pokemons', (req, res)=>{
+    const newPokemon = {
+        name: req.body.name,
+        img: req.body.img,
+        type: req.body.type,
+        stats: {
+            hp: req.body.hp,
+            attack: req.body.attack,
+            defense: req.body.defense,
+            spattack: req.body.spattack,
+            spdefense: req.body.spdefense,
+            speed: req.body.speed,
+        }
+    };
+    pokemon.push(newPokemon);
+    res.redirect('/pokemons/')
+})
+
 // SHOW
 app.get('/pokemons/:id', (req, res) => {
-    res.render('show.ejs', { pokeData: pokemon[req.params.id] });
+    res.render('show.ejs', { index: req.params.id, pokeData: pokemon[req.params.id] });
 });
 
 
